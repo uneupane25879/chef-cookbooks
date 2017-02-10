bde00eb07f10eaa22bc73d11ccf5dacbf4123b99
aamirahmad <aamir.ahmad170@gmail.com>
2017-02-10 08:17:49 +0530

    Adding HTML file

--- nginx/recipes/default.rb
+++ nginx/recipes/default.rb
@@ -6,3 +6,16 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+package 'nginx' do
+  action :install
+end
+
+
+service 'nginx' do
+  action [ :enable, :start]
+end
+
+cookbook_file "/usr/share/nginx/www/index.html" do
+  source 'index.html'
+  mode '0644'
+end
