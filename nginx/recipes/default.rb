97bf6660ec60b966a4ea49d2b1631f8448d58445
Amir Ahmad <aamir.ahmad170@gmail.com>
2017-02-10 08:25:49 +0530

    Update default.rb

--- nginx/recipes/default.rb
+++ nginx/recipes/default.rb
@@ -15,7 +15,7 @@ service 'nginx' do
   action [ :enable, :start]
 end
 
-cookbook_file "/usr/share/nginx/www/index.html" do
+cookbook_file "var/www/html/index.html" do
   source 'index.html'
   mode '0644'
 end
