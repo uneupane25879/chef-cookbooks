c681a3797d90c0e33bfbf71408ad6a4e13eb5b22
aamirahmad <aamir.ahamd170@gmail.com>
2017-03-17 07:14:10 +0530

    Changes

--- main/recipes/default.rb
+++ main/recipes/default.rb
@@ -1,5 +1,35 @@
-#
-# Cookbook:: main
-# Recipe:: default
-#
-# Copyright:: 2017, The Authors, All Rights Reserved.
+node.default['main']['doc_root']="/var/www/html"
+
+execute "apt-get update" 
+	command "apt-get update"
+end
+
+apt_package "apache2"
+	action :install
+end
+
+service "apache2" do
+	action [ :enable, :start]
+end
+
+directory node['main']['doc_root'] do
+	owner 'www-data'
+	group 'www-data'
+	mode '0644'
+	action :create
+end
+
+cookbook_file "#{node['main']['doc_root']}/index.html" do
+	source 'index.html'
+	owner 'www-data'
+	group 'www-data'
+	action :create
+end
+
+
+template "/etc/apache2/sites-available/000-default.conf" do
+	source "vhost.erb"
+	variables({ :doc_root => node['main']['doc_root']})
+	action:create
+	notifies :restart, resources(:service => "apache2")
+end
